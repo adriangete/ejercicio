b76206d5b19be73ac64fd02ee0f8f158b71f3b6b
adriangete <adrian.getroj@educa.jcyl.es>
2021-09-28 11:31:54 +0200

    first commit

---
+++ funciones/funciones.js
@@ -0,0 +1,16 @@
+        window.onload = function(){
+            alert("Load");
+        };
+        
+        let elemento=document.getElementById("nombre");
+        nombre.addEventListener("mouseover", function () {
+
+            nombre.style.color = "green";
+            nombre.innerText = (nombre.innerText).toUpperCase();
+
+        }, false)
+        nombre.addEventListener("mouseout", function () {
+            nombre.style.color = "black";
+            nombre.innerText = (nombre.innerText).toLowerCase();
+
+        }, false)
